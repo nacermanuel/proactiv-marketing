b70c72153f0f639075c8c5458f4c148891816b12
Manuel <nacermanuel@gmail.com>
2023-06-06 16:43:15 -0500

    comp4

--- app/components/Comp04.js
+++ app/components/Comp04.js
@@ -2,12 +2,10 @@
 
 const Comp04 = () => {
 
-
-
   return (
     <div className="flex flex-col justify-center items-center  h-full w-[100vw]">
         <p className="fontTitle">Artworks and Delivery Explained</p>
-        <video controls className="max-h-[90vh]">
+        <video controls className="h-[90vh] border-3 border-red-400">
           <source src={'https://res.cloudinary.com/dbhtt5ozr/video/upload/v1686066069/VIDEO_3_FINAL_v3_wau6e4.mp4'} type="video/mp4" />
           Your browser does not support the video tag.
         </video>
