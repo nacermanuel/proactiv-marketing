47aecc31068e13626a273bbdb65bffb720ca9084
Manuel <nacermanuel@gmail.com>
2023-06-06 16:21:36 -0500

    comp4

--- app/components/Comp04.js
+++ app/components/Comp04.js
@@ -5,13 +5,15 @@ const Comp04 = () => {
 
 
   return (
+    <div className="flex flex-col justify-center items-center  h-full w-[100vw]">
+        <p className="fontTitle">Artworks and Delivery Explained</p>
+        <video controls className="max-h-[90vh]">
+          <source src={'https://res.cloudinary.com/dbhtt5ozr/video/upload/v1686066069/VIDEO_3_FINAL_v3_wau6e4.mp4'} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
 
-    <div className="max-h-[100vh] ">
-      <video controls className="max-h-full">
-        <source src={'https://res.cloudinary.com/dbhtt5ozr/video/upload/v1686066069/VIDEO_3_FINAL_v3_wau6e4.mp4'} type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
     </div>
+
     
   )
 }
