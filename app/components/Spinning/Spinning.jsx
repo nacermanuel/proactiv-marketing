67d7b1323a42b67abed7d70444b24dca3f057ada
Manuel <nacermanuel@gmail.com>
2023-06-07 00:06:39 -0500

    spinningwheel style

---
+++ app/components/Spinning/Spinning.jsx
@@ -0,0 +1,64 @@
+'use client'
+import React, { useEffect, useState } from 'react'
+import './Spinning.css'
+
+
+export const Spinning = () => {
+  const [deg, setDeg] = useState(0)
+  const [calc, setCalc] =useState(0)
+  const [prize, setPrize] = useState('')
+
+  const handleSpin = () => {
+    setDeg(Math.ceil(Math.random() * 100000))
+  }
+
+  useEffect(()=>{
+    setCalc(deg%360)
+  },[deg])
+
+  useEffect(()=>{
+
+    if (calc >= 0 && calc <= 29) {
+      setPrize("3 months FREE");
+    } else if (calc >= 30 && calc <= 89) {
+      setPrize("12 months FREE");
+    } else if (calc >= 90 && calc <= 149) {
+      setPrize("9 months FREE");
+    } else if (calc >= 150 && calc <= 209) {
+      setPrize("6 months FREE + 6 months half price");
+    } else if (calc >= 210 && calc <= 269) {
+      setPrize("6 months FREE");
+    } else if (calc >= 270 && calc <= 329) {
+      setPrize("3 months FREE + 3 months half price");
+    } else if (calc >= 330 && calc <= 359) {
+      setPrize("3 months FREE");
+    }
+    
+    
+  },[calc])
+
+  return (
+
+    <div className="flex flex-col justify-center items-center w-full min-h-screen">
+      <p className="fontTitle">SPINWHEEL</p>
+      <div id='containerSpinner' className='h-auto w-[95%] flex flex-col justify-center items-center'>
+        <p>{prize}</p>
+        <div className='flex justify-center items-center my-7 px-2 transform scale-100'>
+          <div class="containerCSS" style={{transform: `rotate(${deg}deg)`}}>
+            <div class="containerDIV containerONE"><div className='text-center w-[42px] h-[115px]'> 3 months FREE</div></div>
+            <div class="containerDIV containerTWO"><div className='text-center w-[42px] h-[115px]'> 3M free + 3M <p className='text-sm inline'>1/2</p> price </div></div>
+            <div class="containerDIV containerTHREE"><div className='text-center w-[42px] h-[115px]'>6 months FREE</div></div>
+            <div class="containerDIV containerFOUR"><div className='text-center w-[42px] h-[115px]'>6M free + 6M <p className='text-sm inline'>1/2</p>  price</div></div>
+            <div class="containerDIV containerFIVE"><div className='text-center w-[42px] h-[115px]'>9 months FREE</div></div>
+            <div class="containerDIV containerSIX"><div className='text-center w-[42px] h-[115px]'>12 months FREE</div></div>
+          </div>
+          <span class="mid"></span>
+          <div class="stoper"></div>
+        </div>    
+      </div>
+      <button className='buttonsMain' onClick={handleSpin} >Spin</button>
+
+    </div>
+
+  )
+}
