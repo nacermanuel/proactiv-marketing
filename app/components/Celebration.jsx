f7a6d475ccdb5ed43851c1fa23a858847888ca5f
Manuel <nacermanuel@gmail.com>
2023-06-08 23:03:28 -0500

    Congratulation 1

--- app/components/Celebration.jsx
+++ app/components/Celebration.jsx
@@ -1,19 +1,34 @@
 'use client'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Confetti from 'react-confetti'
 
 const Celebration = () => {
-    const { width, height } = useWindowSize()
-  return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
 
+    const [width , setWidth] =useState(0)
+    const [height , setHeight] =useState(0)
+    
+    let { width1, height1 } = useWindowSize()
+    
+    useEffect(()=>{
+        setWidth(width1)
+        setHeight(height1)
+    },[])
+
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-[#ccffee]">
+
+        <p className='fontTitle'>Congratulations!</p>
+        <img src="https://media.giphy.com/media/BrFuiMe3YUt3laSeEO/giphy.gif" alt="" />
+        <p>we can replace the GIF by any in this web: <a href="https://giphy.com/" target="_blank">https://giphy.com/</a> </p>
         
 
         {   width == 'Infinity' || height == 'Infinity' ? null :
             <Confetti
                 width={width}
                 height={height}
+                gravity={0.3}
+                numberOfPieces={400}
             />
         }
 
