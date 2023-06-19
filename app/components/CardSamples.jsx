5fab83afe4a1f9c6841540b9ce3f2ffc9275d95c
Manuel <nacermanuel@gmail.com>
2023-06-19 13:05:29 -0500

    new samples

--- app/components/CardSamples.jsx
+++ app/components/CardSamples.jsx
@@ -8,16 +8,22 @@ const CardSamples = () => {
 
 
         <p className="fontTitle">Your Cards Represent Your Business</p>
-        <div className="flex flex-row flex-wrap justify-around items-center">
-                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1686157767/CS7_lgs3ja.jpg" alt="" />
+        <div className="flex flex-row flex-wrap justify-around items-center z-[1]">
+                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1687197160/Sin_t%C3%ADtulo-1_Mesa_de_trabajo_1_copia_2_fhirv3.png" alt="" /> 
                 <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1686157364/CS4_oaeyvy.jpg" alt="" />
 
+                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1687197160/Sin_t%C3%ADtulo-1_Mesa_de_trabajo_1_copia_3_amaojv.png" alt="" /> 
+                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1686157364/CS5_vgxtcv.png" alt="" />
 
-                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1686157364/CS5_vgxtcv.png" alt="" />
-                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1686157364/CS3_gwphcx.jpg" alt="" />                  
+                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1686157364/CS3_gwphcx.jpg" alt="" />                  
+                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1687197160/Sin_t%C3%ADtulo-1_Mesa_de_trabajo_1_xkkal3.png" alt="" />
 
-                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1686157364/CS2_su3sie.jpg" alt="" />
-                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1686157364/CS6_zurcdm.jpg" alt="" /> 
+                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1686157364/CS6_zurcdm.jpg" alt="" /> 
+                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1687197160/Sin_t%C3%ADtulo-1_Mesa_de_trabajo_1_copia_4_s09qpv.png" alt="" /> 
+
+                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageLeft" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1687197160/Sin_t%C3%ADtulo-1_Mesa_de_trabajo_1_copia_hra8f5.png" alt="" /> 
+                <img className="w-[150px] h-[150px] overflow-hidden pb-3 rounded-lg samples-zoomable-imageRight" src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1686157767/CS7_lgs3ja.jpg" alt="" />
+
 
             {/* <div className="flex flex-col justify-between items-center">
                 <img src="https://res.cloudinary.com/dbhtt5ozr/image/upload/v1686157767/CS7_lgs3ja.jpg" alt="" />
